6046a60c5ff0c9dbffc468ffdbf0bb66c81d5955
Niket Patel <niket.patel@insightmethods.com>
2011-05-16 20:04:02 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Syweb::Application.config.secret_token = 'd4a353b56f1b8b3f3f2c7b073ceaf206fdac3e1572b0eaabba0049b97ffa71631b02221a8fec7aea6c771949823d8dd6c4b5670a89977ef620e69821929eb996'
